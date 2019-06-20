0e8424a1eada773ad3251f115995b3a601dc0e32
Hyunsu <hyunsu@HyunsudeMacBook-Pro.local>
2019-06-20 14:16:39 +0800

    清楚测试信息

--- application/Event.php
+++ application/Event.php
@@ -4,13 +4,11 @@
 namespace app;
 
 
-use yun\exception\Exception;
-
 class Event extends \yun\console\Event
 {
     public function onWorkerStart($businessWorker)
     {
-        echo 1 / 0;
+
     }
 
     public function onConnect($client_id)
